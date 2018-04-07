b9d5ac9bc46a250a3a2b239963822f8cf58fd838
75761.vps.ihc.ru <vostok8bit@yandex.ru>
2018-04-07 17:57:39 +0300

    Initial Commit

---
+++ www/constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Constants
+
+# Токены
+token_Transerfing = '444113236:AAFnQec_qg6eAoK6kQPw97B6KG6TkNPqUso'
+token_Nick420 = '339269689:AAH539T6KbaCxPp-MA3XMEozwCMNEqnca0U'
+token = token_Transerfing
+
+# ID чатов
+chat_nick = '119245728'
